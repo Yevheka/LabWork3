f9058e362f642feaf12ffe1309eb5d8a45fbda3b
Yevheka <yevheniivysokos@gmail.com>
2025-05-09 23:10:20 +0200

    little changes

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Score baseScore = new ApplicantScore(150, 160, 170);
 
-        // Додати бонус за олімпіаду
         Score withOlympiad = new OlympiadBonus(baseScore);
 
-        // Додати бонус за цільове направлення
         Score finalScore = new TargetBonus(withOlympiad);
 
         System.out.println("Загальна кількість балів з усіма бонусами: " + finalScore.calculate());
